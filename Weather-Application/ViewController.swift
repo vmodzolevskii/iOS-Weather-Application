d60b8c2de71bd4aadcfb5eb0bb939b3be89ffe7c
vmodzolevskii <vmodzolevskii@gmail.com>
2020-09-18 01:59:20 +0300

    Initial Commit

---
+++ Weather-Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather-Application
+//
+//  Created by sdf on 9/18/20.
+//  Copyright © 2020 vmodzolevskii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
