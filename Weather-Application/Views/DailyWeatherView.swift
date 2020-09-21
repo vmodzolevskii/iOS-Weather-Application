190fd6db0e551092fd5456203e64a878e867013f
vmodzolevskii <vmodzolevskii@gmail.com>
2020-09-22 02:47:14 +0300

    refactoring todayView

--- Weather-Application/Views/DailyWeatherView.swift
+++ Weather-Application/Views/DailyWeatherView.swift
@@ -16,16 +16,11 @@ class DailyWeatherView: UIView {
     
     let titleView: UIView = {
         let view = UIView()
-        view.layer.borderWidth = 1.0
-        view.layer.borderColor = UIColor.lightGray.cgColor
-        view.layer.backgroundColor = UIColor.yellow.cgColor
         return view
     }()
     
     let mainInfoView: UIView = {
         let view = UIView()
-        view.layer.borderWidth = 1.0
-        view.layer.backgroundColor = UIColor.green.cgColor
         return view
     }()
     
@@ -34,14 +29,13 @@ class DailyWeatherView: UIView {
     
     let detailsInfoView = UIView()
     
-    let firstValue = UILabel()
-    let secondValue = UILabel()
-    let thirdValue = UILabel()
-    let fourthValue = UILabel()
-    let fivestValue = UILabel()
+    // details of the weather
+    var characteristicLabels = [UILabel]()
     
     let shareView = UIView()
     let shareButton = UIButton()
+    
+    var valueUnits = [String]()
     
     let viewSeparator: UIView = {
         let view = UIView()
@@ -84,7 +78,7 @@ class DailyWeatherView: UIView {
         self.addSubview(mainInfoView)
         mainInfoView.snp.makeConstraints { make in
             make.left.right.equalToSuperview()
-            make.top.equalTo(titleView.snp.bottom)
+            make.top.equalTo(titleView.snp.bottom).offset(screenHeight / 16)
             make.height.equalTo(screenHeight / 4)
         }
         
@@ -100,7 +94,7 @@ class DailyWeatherView: UIView {
         
         setupDetailsInfoView()
         self.addSubview(detailsInfoView)
-        detailsInfoView.backgroundColor = UIColor.red
+        
         detailsInfoView.snp.makeConstraints { make in
             make.left.right.equalToSuperview()
             make.top.equalTo(mainInfoView.snp.bottom).offset(40)
@@ -128,9 +122,10 @@ class DailyWeatherView: UIView {
             make.top.equalTo(oneMoreSeparator.snp.bottom).offset(5)
             make.height.equalTo(screenHeight / 4)
         }
-        //shareButton.setTitle("Share", for: .disabled)
+        
         shareButton.setTitle("Share", for: .normal)
-        shareButton.backgroundColor = UIColor.black
+        shareButton.setTitleColor(UIColor.orange, for: .normal)
+        shareButton.backgroundColor = UIColor.white
         
         
         shareButton.snp.makeConstraints { make in
@@ -150,7 +145,9 @@ class DailyWeatherView: UIView {
             make.top.equalToSuperview()
         }
         
+        locationTitle.font = UIFont.systemFont(ofSize: 18)
         locationTitle.text = "Location placeholder"
+        temperatureTitle.font = UIFont.systemFont(ofSize: 18)
         temperatureTitle.text = "temp placeholder"
         
         mainInfoView.addSubview(locationTitle)
@@ -167,88 +164,77 @@ class DailyWeatherView: UIView {
         }
     }
     
+    func pushUnits() {
+        valueUnits.append("%")
+        valueUnits.append(" mm")
+        valueUnits.append(" hPa")
+        valueUnits.append(" km/h")
+        valueUnits.append("")
+    }
+    
+    func characteristicImageView(with name: String) -> UIImageView {
+        let image = UIImage(named: name)
+        let imageView = UIImageView(image: image)
+        return imageView
+    }
+    
     func setupDetailsInfoView() {
-        firstValue.text = "42%"
-        secondValue.text = "42 mm"
-        thirdValue.text = "42 hPa"
-        fourthValue.text = "42 km/h"
-        fivestValue.text = "SE"
+        pushUnits()
         
-        let image1 = UIImage(named: "Placeholder")
-        let image2 = UIImage(named: "Placeholder")
-        let image3 = UIImage(named: "Placeholder")
-        let image4 = UIImage(named: "Placeholder")
-        let image5 = UIImage(named: "Placeholder")
+        var characteristicLabels = [UILabel]()
+        var characteristicViews = [UIImageView]()
         
-        let imageView1 = UIImageView(image: image1)
-        let imageView2 = UIImageView(image: image2)
-        let imageView3 = UIImageView(image: image3)
-        let imageView4 = UIImageView(image: image4)
-        let imageView5 = UIImageView(image: image5)
-        
-        detailsInfoView.addSubview(imageView1)
-        detailsInfoView.addSubview(imageView2)
-        detailsInfoView.addSubview(imageView3)
-        detailsInfoView.addSubview(imageView4)
-        detailsInfoView.addSubview(imageView5)
-        
-        detailsInfoView.addSubview(firstValue)
-        detailsInfoView.addSubview(secondValue)
-        detailsInfoView.addSubview(thirdValue)
-        detailsInfoView.addSubview(fourthValue)
-        detailsInfoView.addSubview(fivestValue)
-        
-        
-        // first row
-        imageView1.snp.makeConstraints { make in
-            make.height.width.equalTo(30)
-            make.left.equalTo(detailsInfoView).offset(60)
-        }
-        imageView2.snp.makeConstraints { make in
-            make.height.width.equalTo(30)
-            make.centerX.equalTo(detailsInfoView)
-        }
-        imageView3.snp.makeConstraints { make in
-            make.height.width.equalTo(30)
-            make.right.equalTo(detailsInfoView).offset(-60)
+        for i in 0..<5 {
+            let label = UILabel()
+            label.text = "42" + valueUnits[i]
+            characteristicLabels.append(label)
+            
+            characteristicViews.append(characteristicImageView(with: "Placeholder"))
+            
+            detailsInfoView.addSubview(characteristicViews[i])
+            detailsInfoView.addSubview(characteristicLabels[i])
         }
         
-        firstValue.snp.makeConstraints { make in
-            make.left.equalTo(detailsInfoView).offset(60)
-            make.top.equalTo(imageView1.snp.bottom)
-        }
+        let iconWidthHeight = screenWidth / 12
         
-        secondValue.snp.makeConstraints { make in
-            make.centerX.equalTo(detailsInfoView)
-            make.top.equalTo(imageView2.snp.bottom)
-        }
+        // first row of weather's details
+        makeImageViewConstraints(for: characteristicViews[0], width: Int(iconWidthHeight), height: Int(iconWidthHeight), topConstraint: nil, verticalOffset: nil, leftConstraint: detailsInfoView, horizontalOffset: Int(screenWidth) / 5)
         
-        thirdValue.snp.makeConstraints { make in
-            make.right.equalTo(detailsInfoView).offset(-60)
-            make.top.equalTo(imageView3.snp.bottom)
-        }
+        makeImageViewConstraints(for: characteristicViews[1], width: Int(iconWidthHeight), height: Int(iconWidthHeight), topConstraint: nil, verticalOffset: nil, leftConstraint: detailsInfoView, horizontalOffset: nil)
+        
+        makeImageViewConstraints(for: characteristicViews[2], width: Int(iconWidthHeight), height: Int(iconWidthHeight), topConstraint: nil, verticalOffset: nil, leftConstraint: detailsInfoView, horizontalOffset: Int(screenWidth) * 4 / 5 - Int(iconWidthHeight))
+        
+        makeImageViewConstraints(for: characteristicLabels[0], width: nil, height: nil, topConstraint: characteristicViews[0], verticalOffset: 0, leftConstraint: detailsInfoView, horizontalOffset: Int(screenWidth) / 5)
+        
+        makeImageViewConstraints(for: characteristicLabels[1], width: nil, height: nil, topConstraint: characteristicViews[1], verticalOffset: 0, leftConstraint: detailsInfoView, horizontalOffset: nil)
+        
+        makeImageViewConstraints(for: characteristicLabels[2], width: nil, height: nil, topConstraint: characteristicViews[2], verticalOffset: 0, leftConstraint: detailsInfoView, horizontalOffset: Int(screenWidth) * 4 / 5 - Int(iconWidthHeight))
+        
         
         // second row
-        imageView4.snp.makeConstraints { make in
-            make.height.width.equalTo(30)
-            make.top.equalTo(firstValue.snp.bottom).offset(20)
-            make.left.equalTo(detailsInfoView).offset(120)
-        }
+        makeImageViewConstraints(for: characteristicViews[3], width: Int(iconWidthHeight), height: Int(iconWidthHeight), topConstraint: characteristicViews[0], verticalOffset: Int(screenHeight) / 20, leftConstraint: detailsInfoView, horizontalOffset: Int(screenWidth) * 1 / 3)
         
-        imageView5.snp.makeConstraints { make in
-            make.height.width.equalTo(30)
-            make.top.equalTo(firstValue.snp.bottom).offset(20)
-            make.right.equalTo(detailsInfoView).offset(-120)
-        }
+        makeImageViewConstraints(for: characteristicViews[4], width: Int(iconWidthHeight), height: Int(iconWidthHeight), topConstraint: characteristicViews[0], verticalOffset: Int(screenHeight) / 20, leftConstraint: detailsInfoView, horizontalOffset: (Int(screenWidth) * 2 / 3 - Int(iconWidthHeight)))
         
-        fourthValue.snp.makeConstraints { make in
-            make.top.equalTo(imageView4.snp.bottom)
-            make.left.equalTo(detailsInfoView).offset(120)
-        }
+        makeImageViewConstraints(for: characteristicLabels[3], width: nil, height: nil, topConstraint: characteristicViews[3], verticalOffset: 0, leftConstraint: detailsInfoView, horizontalOffset: (Int(screenWidth) * 1 / 3 - Int(iconWidthHeight)))
         
-        fivestValue.snp.makeConstraints { make in
-            make.top.equalTo(imageView5.snp.bottom)
-            make.right.equalTo(detailsInfoView).offset(-120)
+        makeImageViewConstraints(for: characteristicLabels[4], width: nil, height: nil, topConstraint: characteristicViews[4], verticalOffset: 0, leftConstraint: detailsInfoView, horizontalOffset: (Int(screenWidth) * 2 / 3 - Int(iconWidthHeight)))
+    }
+    
+    func makeImageViewConstraints(for view: UIView, width: Int?, height: Int?, topConstraint: UIView?, verticalOffset: Int?, leftConstraint: UIView, horizontalOffset: Int?) {
+        view.snp.makeConstraints { make in
+            if let widthValue = width, let heightValue = height {
+                make.height.equalTo(heightValue)
+                make.width.equalTo(widthValue)
+            }
+            if let topView = topConstraint, let vertOffset = verticalOffset {
+                make.top.equalTo(topView.snp.bottom).offset(vertOffset)
+            }
+            if let horOffset = horizontalOffset {
+                make.left.equalTo(leftConstraint).offset(horOffset)
+            } else {
+                make.centerX.equalTo(leftConstraint)
+            }
         }
     }
     
