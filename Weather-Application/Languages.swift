e8fe18103c8a672e773d900450a7a6630817496c
vmodzolevskii <vmodzolevskii@gmail.com>
2020-09-24 00:28:55 +0300

    added pictures, created language dictionary

---
+++ Weather-Application/Languages.swift
@@ -0,0 +1,62 @@
+//
+//  Languages.swift
+//  Weather-Application
+//
+//  Created by sdf on 9/23/20.
+//  Copyright © 2020 vmodzolevskii. All rights reserved.
+//
+
+import Foundation
+
+class Language {
+    
+public static let languages = [
+    "by": "Belarus",
+    "af": "Afrikaan",
+    "al": "Albania",
+    "ar": "Arabic",
+    "az": "Azerbaijani",
+    "bg": "Bulgaria",
+    "ca": "Catalan",
+    "cz": "Czech",
+    "da": "Danish",
+    "de": "Germany",
+    "el": "Greek",
+    "en": "English",
+    "eu": "Basque",
+    "fa": "Persian",
+    "fi": "Finnish",
+    "fr": "French",
+    "gl": "Galician",
+    "he": "Hebrew",
+    "hi": "Hindi",
+    "hr": "Croatian",
+    "hu": "Hungarian",
+    "id": "Indonesian",
+    "it": "Italian",
+    "ja": "Japanese",
+    "kr": "Korean",
+    "la": "Latvian",
+    "lt": "Lithuanian",
+    "mk": "Macedonian",
+    "no": "Norwegian",
+    "nl": "Dutch",
+    "pl": "Polish",
+    "pt": "Portuguese",
+    "pt_br": "Português Brasil",
+    "ro": "Romanian",
+    "ru": "Russia",
+    "sv": "Swedish",
+    "sk": "Slovak",
+    "sl": "Slovenian",
+    "sp": "Spanish",
+    "sr": "Serbian",
+    "th": "Thai",
+    "tr": "Turkish",
+    "ua": "Ukrainian",
+    "vi": "Vietnamese",
+    "zh_cn": "Chinese Simplified",
+    "zh_tw": "Chinese Traditional",
+    "zu": "Zulu"]
+
+}
